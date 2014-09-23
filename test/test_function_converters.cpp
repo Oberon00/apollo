9a047a7d07609b3dd27b6ce28b82fff3bed79267
Christian Neumüller <cn00@gmx.at>
2014-09-23 12:55:53 +0200

    Add test_function_converters.

---
+++ test/test_function_converters.cpp
@@ -0,0 +1,217 @@
+#include <boost/function.hpp>
+#include <apollo/function.hpp>
+#include <string>
+
+#include "test_prefix.hpp"
+
+static unsigned g_n_calls = 0;
+
+static void proc0()
+{
+    ++g_n_calls;
+}
+
+static void proc1(int a)
+{
+    BOOST_CHECK_EQUAL(a, 42);
+    ++g_n_calls;
+}
+
+static void proc4(int a, std::string const& s, double d, bool b)
+{
+    BOOST_CHECK_EQUAL(a, 42);
+    BOOST_CHECK_EQUAL(s, "foo");
+    BOOST_CHECK_EQUAL(d, 3.14);
+    BOOST_CHECK_EQUAL(b, false);
+    ++g_n_calls;
+}
+
+BOOST_AUTO_TEST_CASE(plain_proc)
+{
+    g_n_calls = 0;
+    apollo::push(L, &proc0);
+    lua_pushvalue(L, -1); // Copy, so that one is left after pcall.
+    apollo::pcall(L, 0, 0);
+    BOOST_REQUIRE_EQUAL(g_n_calls, 1u);
+    BOOST_REQUIRE(apollo::is_convertible<decltype(&proc0)>(L, -1));
+    auto proc0ptr = apollo::from_stack<decltype(&proc0)>(L, -1);
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(proc0ptr, &proc0);
+
+    apollo::push(L, &proc1);
+    lua_pushvalue(L, -1); // Copy, so that one is left after pcall.
+    lua_pushinteger(L, 42);
+    apollo::pcall(L, 1, 0);
+    BOOST_REQUIRE_EQUAL(g_n_calls, 2u);
+    BOOST_REQUIRE(apollo::is_convertible<decltype(&proc1)>(L, -1));
+    auto proc1ptr = apollo::from_stack<decltype(&proc1)>(L, -1);
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(proc1ptr, &proc1);
+
+    apollo::push(L, &proc4);
+    lua_pushvalue(L, -1); // Copy, so that one is left after pcall.
+    lua_pushinteger(L, 42);
+    lua_pushliteral(L, "foo");
+    lua_pushnumber(L, 3.14);
+    lua_pushboolean(L, false);
+    apollo::pcall(L, 4, 0);
+    BOOST_REQUIRE_EQUAL(g_n_calls, 3u);
+    BOOST_REQUIRE(apollo::is_convertible<decltype(&proc4)>(L, -1));
+    auto proc4ptr = apollo::from_stack<decltype(&proc4)>(L, -1);
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(proc4ptr, &proc4);
+}
+
+static unsigned func0()
+{
+    return ++g_n_calls;
+}
+
+static char const* func1(int a)
+{
+    ++g_n_calls;
+    BOOST_CHECK_EQUAL(a, 42);
+    return "foo";
+}
+
+static bool func4(int a, std::string const& s, double d, bool b)
+{
+    ++g_n_calls;
+    BOOST_CHECK_EQUAL(a, 42);
+    BOOST_CHECK_EQUAL(s, "foo");
+    BOOST_CHECK_EQUAL(d, 3.14);
+    BOOST_CHECK_EQUAL(b, false);
+    return b;
+}
+
+BOOST_AUTO_TEST_CASE(plain_func)
+{
+    g_n_calls = 0;
+    apollo::push(L, &func0);
+    lua_pushvalue(L, -1); // Copy, so that one is left after pcall.
+    apollo::pcall(L, 0, 1);
+    BOOST_REQUIRE_EQUAL(g_n_calls, 1u);
+    BOOST_CHECK_EQUAL(static_cast<unsigned>(lua_tointeger(L, -1)), g_n_calls);
+    lua_pop(L, 1);
+    BOOST_REQUIRE(apollo::is_convertible<decltype(&func0)>(L, -1));
+    auto func0ptr = apollo::from_stack<decltype(&func0)>(L, -1);
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(func0ptr, &func0);
+
+    apollo::push(L, &func1);
+    lua_pushvalue(L, -1); // Copy, so that one is left after pcall.
+    lua_pushinteger(L, 42);
+    apollo::pcall(L, 1, 1);
+    BOOST_REQUIRE_EQUAL(g_n_calls, 2u);
+    BOOST_CHECK_EQUAL(apollo::from_stack<std::string>(L, -1), "foo");
+    lua_pop(L, 1);
+    BOOST_REQUIRE(apollo::is_convertible<decltype(&func1)>(L, -1));
+    auto func1ptr = apollo::from_stack<decltype(&func1)>(L, -1);
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(func1ptr, &func1);
+
+    apollo::push(L, &func4);
+    lua_pushvalue(L, -1); // Copy, so that one is left after pcall.
+    lua_pushinteger(L, 42);
+    lua_pushliteral(L, "foo");
+    lua_pushnumber(L, 3.14);
+    lua_pushboolean(L, false);
+    apollo::pcall(L, 4, 1);
+    BOOST_REQUIRE_EQUAL(g_n_calls, 3u);
+    BOOST_CHECK_EQUAL(lua_type(L, -1), LUA_TBOOLEAN);
+    BOOST_CHECK_EQUAL(lua_toboolean(L, -1) ? true : false, false);
+    lua_pop(L, 1);
+    BOOST_REQUIRE(apollo::is_convertible<decltype(&func4)>(L, -1));
+    auto func4ptr = apollo::from_stack<decltype(&func4)>(L, -1);
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(func4ptr, &func4);
+}
+
+static void check_proc(lua_State* L)
+{
+    g_n_calls = 0;
+
+    auto proc0obj = apollo::from_stack<std::function<void()>>(L, -1);
+    lua_pop(L, 1);
+    proc0obj();
+    BOOST_CHECK_EQUAL(g_n_calls, 1u);
+
+    apollo::push(L, proc0obj);
+    auto proc0objb = apollo::from_stack<boost::function<void()>>(L, -1);
+    proc0objb();
+    BOOST_CHECK_EQUAL(g_n_calls, 2u);
+    apollo::pcall(L, 0, 0);
+    BOOST_CHECK_EQUAL(g_n_calls, 3u);
+
+    apollo::push(L, proc0objb);
+    proc0obj = apollo::from_stack<std::function<void()>>(L, -1);
+    proc0obj();
+    BOOST_CHECK_EQUAL(g_n_calls, 4u);
+    apollo::pcall(L, 0, 0);
+    BOOST_CHECK_EQUAL(g_n_calls, 5u);
+}
+
+BOOST_AUTO_TEST_CASE(proc_obj)
+{
+    apollo::push(L, &proc0);
+    check_proc(L);
+
+    struct proc_obj {
+        void operator() ()
+        {
+            proc0();
+        }
+    };
+    std::function<void()> pobj(proc_obj{});
+    apollo::push(L, pobj);
+    check_proc(L);
+}
+
+static void check_func(lua_State* L)
+{
+    g_n_calls = 0;
+
+    auto func1obj = apollo::from_stack<std::function<char const*(int)>>(L, -1);
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(func1obj(42), std::string("foo"));
+    BOOST_CHECK_EQUAL(g_n_calls, 1u);
+
+    apollo::push(L, func1obj);
+    auto func1objb = apollo::from_stack<boost::function<char const*(int)>>(L, -1);
+    BOOST_CHECK_EQUAL(func1objb(42), std::string("foo"));
+    BOOST_CHECK_EQUAL(g_n_calls, 2u);
+    lua_pushinteger(L, 42);
+    apollo::pcall(L, 1, 1);
+    BOOST_CHECK_EQUAL(lua_tostring(L, -1), std::string("foo"));
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(g_n_calls, 3u);
+
+    apollo::push(L, func1objb);
+    func1obj = apollo::from_stack<std::function<char const*(int)>>(L, -1);
+    BOOST_CHECK_EQUAL(func1obj(42), std::string("foo"));
+    BOOST_CHECK_EQUAL(g_n_calls, 4u);
+    lua_pushinteger(L, 42);
+    apollo::pcall(L, 1, 1);
+    BOOST_CHECK_EQUAL(lua_tostring(L, -1), std::string("foo"));
+    lua_pop(L, 1);
+    BOOST_CHECK_EQUAL(g_n_calls, 5u);
+}
+
+BOOST_AUTO_TEST_CASE(func_obj)
+{
+    apollo::push(L, &func1);
+    check_func(L);
+
+    struct func_obj {
+        char const* operator() (int a)
+        {
+            return func1(a);
+        }
+    };
+    std::function<char const*(int)> fobj(func_obj{});
+    apollo::push(L, fobj);
+    check_func(L);
+}
+
+
+#include "test_suffix.hpp"
