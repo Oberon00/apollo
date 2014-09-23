e6cdd8d6af46f7e80adb65abcc2d5e7442b2455b
Christian Neumüller <cn00@gmx.at>
2014-09-23 12:51:28 +0200

    push_gc_objcect: Pop in case of ctor exceptions.

--- include/apollo/gc.hpp
+++ include/apollo/gc.hpp
@@ -22,7 +22,12 @@ push_gc_object(lua_State* L, T&& o)
 {
     typedef typename detail::remove_qualifiers<T>::type obj_t;
     void* uf = lua_newuserdata(L, sizeof(obj_t));
-    new(uf) obj_t(std::forward<T>(o)); // TODO: Balance stack in case of exceptions.
+    try {
+        new(uf) obj_t(std::forward<T>(o));
+    } catch (...) {
+        lua_pop(L, 1);
+        throw;
+    }
     lua_createtable(L, 0, 1);
     lua_pushcfunction(L, &gc_object<obj_t>);
     lua_setfield(L, -2, "__gc");
