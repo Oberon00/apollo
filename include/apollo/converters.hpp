c795ed7463459f14a44c68217b043d9d6abd15f2
Christian Neumüller <cn00@gmx.at>
2014-10-14 20:39:15 +0200

    Add helper function add_conversion_step.

--- include/apollo/converters.hpp
+++ include/apollo/converters.hpp
@@ -70,6 +70,11 @@ struct converter;
 
 BOOST_CONSTEXPR_OR_CONST unsigned no_conversion = UINT_MAX;
 
+inline BOOST_CONSTEXPR unsigned add_conversion_step(unsigned n_steps)
+{
+    return n_steps == no_conversion ? no_conversion : n_steps + 1;
+}
+
 template<typename T>
 struct converter_base {
     using type = T;
