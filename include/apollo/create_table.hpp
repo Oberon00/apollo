0055be8202dcb43af82058d877cf23a21a19266d
Christian Neumüller <cn00@gmx.at>
2014-10-18 12:05:56 +0200

    create_table: Support subtables.

--- include/apollo/create_table.hpp
+++ include/apollo/create_table.hpp
@@ -1,7 +1,12 @@
 #ifndef APOLLO_CREATE_TABLE_HPP_INCLUDED
 #define APOLLO_CREATE_TABLE_HPP_INCLUDED APOLLO_CREATE_TABLE_HPP_INCLUDED
 
+#include <apollo/converters.hpp>
+
 #include <lua.hpp>
+#include <boost/assert.hpp>
+
+#include <set>
 
 namespace apollo {
 
@@ -9,41 +14,88 @@ namespace detail {
 
 class table_setter {
 public:
-    table_setter(lua_State* L, int table_idx)
-        : m_L(L), m_table_idx(table_idx) {}
+    table_setter(lua_State* L, int table_idx, bool newtable)
+        : m_L(L), m_table_idx(table_idx), m_newtable(newtable) {}
+
+    ~table_setter()
+    {
+        for (auto it = m_pending_removals.rbegin();
+             it != m_pending_removals.rend();
+             ++it
+        ) {
+            lua_remove(m_L, *it);
+        }
+    }
+
+    table_setter(table_setter const&) = default; // Use with care!
 
     template <typename K, typename V>
-    table_setter& operator() (K&& key, V&& value)
+    table_setter&& operator() (K&& key, V&& value)
     {
         apollo::push(m_L, std::forward<K>(key), std::forward<V>(value));
         lua_rawset(m_L, m_table_idx);
-        return *this;
+        return std::move(*this);
     }
 
     template <typename V>
-    table_setter& operator() (int key, V&& value)
+    table_setter&& operator() (int key, V&& value)
     {
         apollo::push(m_L, std::forward<V>(value));
         lua_rawseti(m_L, m_table_idx, key);
-        return *this;
+        return std::move(*this);
     }
 
+    template <typename K>
+    table_setter&& operator() (K&& key, table_setter&& value)
+    {
+        apollo::push(m_L, std::forward<K>(key));
+        consume_table(std::move(value));
+        lua_rawset(m_L, m_table_idx);
+        return std::move(*this);
+    }
+
+    table_setter&& operator() (int key, table_setter&& value)
+    {
+        consume_table(std::move(value));
+        lua_rawseti(m_L, m_table_idx, key);
+        return std::move(*this);
+    }
+
+
 private:
-    lua_State* m_L;
+    void consume_table(table_setter&& value)
+    {
+        BOOST_ASSERT(value.m_L == m_L);
+        if (value.m_table_idx != lua_gettop(m_L)) {
+            lua_pushvalue(m_L, value.m_table_idx);
+            if (value.m_table_idx > 0 && value.m_newtable)
+                m_pending_removals.insert(value.m_table_idx);
+        }
+        m_pending_removals.insert(
+            value.m_pending_removals.begin(),
+            value.m_pending_removals.end());
+        value.m_pending_removals.clear();
+        value.m_table_idx = 0;
+    }
+
+    lua_State* const m_L;
     int m_table_idx;
+    std::set<int> m_pending_removals;
+    bool m_newtable;
 };
 
 } // namespace detail
 
+
 inline detail::table_setter rawset_table(lua_State* L, int table_idx)
 {
-    return {L, lua_absindex(L, table_idx)};
+    return {L, lua_absindex(L, table_idx), false};
 }
 
 inline detail::table_setter new_table(lua_State* L)
 {
     lua_newtable(L);
-    return {L, lua_gettop(L)};
+    return {L, lua_gettop(L), true};
 }
 
 } // namespace apollo
--- test/test_create_table.cpp
+++ test/test_create_table.cpp
@@ -29,17 +29,58 @@ BOOST_AUTO_TEST_CASE(set_table)
     }
 }
 
+static void checktable(lua_State* L)
+{
+    lua_setglobal(L, "t");
+    if (luaL_dostring(L,
+        "assert(type(t) == 'table', 't not a table but ' .. tostring(t))\n"
+        "assert(t[1] == 2, tostring(t[1]) .. ' ~= 2')\n"
+        "assert(t.foo == 'bar', tostring(t.foo) .. ' ~= \"bar\"')\n"
+        "assert(t.sub.add(2, 3) == 5, 'invalid add return value')\n"
+        "assert(t.sub[2] == 42, tostring(t.sub[2]) .. ' ~= 42')\n"
+        "assert(t.sub2[3] == 7, tostring(t.sub2[3]) .. ' ~= 7')\n"
+        "assert(t.sub2.nested.x == 'y',\n"
+        "   tostring(t.sub2.nested.x) .. ' ~= \"y\"')\n"
+        "assert(type(t.sub2.nested[1]) == 'table',\n"
+        "   type(tostring(t.sub2.nested[1])) .. ' ~= \"table\"')\n"
+        "assert(t.sub[3] == nil, tostring(t.sub[3]) .. ' ~= nil')\n"
+        "print(t, t.sub, t.sub2)\n")
+    ) {
+        BOOST_ERROR(lua_tostring(L, -1));
+        lua_pop(L, 1);
+    }
+}
+
 BOOST_AUTO_TEST_CASE(new_table)
 {
+    luaL_requiref(L, "base", &luaopen_base, true);
+    lua_pop(L, 1);
     apollo::new_table(L)
         (1, 2)
-        ("foo", "bar");
-    lua_rawgeti(L, -1, 1);
-    BOOST_CHECK_EQUAL(lua_tointeger(L, -1), 2);
-    lua_pop(L, 1);
-    lua_getfield(L, -1, "foo");
-    BOOST_CHECK_EQUAL(lua_tostring(L, -1), "bar");
-    lua_pop(L, 2);
+        ("foo", "bar")
+        ("sub", apollo::new_table(L)
+            ("add", &add)
+            (2, 42))
+        ("sub2", apollo::new_table(L)
+            (3, 7)
+            ("nested", apollo::new_table(L)
+                ("x", "y")
+                (1, apollo::new_table(L))));
+    checktable(L);
+
+    auto setter = apollo::new_table(L) // Normally not allowed.
+        ("add", &add)
+        (2, 42);
+    apollo::new_table(L)
+        (1, 2)
+        ("foo", "bar")
+        ("sub", std::move(setter))
+        ("sub2", apollo::new_table(L)
+            (3, 7)
+            ("nested", apollo::new_table(L)
+                ("x", "y")
+                (1, apollo::new_table(L))));
+    checktable(L);
 }
 
 #include "test_suffix.hpp"
