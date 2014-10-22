5562290cbb311d1f56b33b82a788ebdd9bec1ca3
Christian Neumüller <cn00@gmx.at>
2014-10-22 21:31:43 +0200

    Travis: Experimental! Do not use!

--- get-deps.sh
+++ get-deps.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -ex
 
-wget http://kent.dl.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2
-tar -xjf boost_1_56_0.tar.bz2
-cd boost_1_56_0
-./bootstrap.sh
-sudo ./b2 install --with-test
+#wget http://kent.dl.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2
+#tar -xjf boost_1_56_0.tar.bz2
+#cd boost_1_56_0
+#./bootstrap.sh
+#sudo ./b2 install --with-test -d0
 
-sudo apt-get install -q -y liblua5.2-dev
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install -q -y liblua5.2-dev libstdc++-4.8-dev
