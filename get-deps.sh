6598e87d2ec02a3c6790f510145ccda4e44ef480
Christian Neumüller <cn00@gmx.at>
2014-10-22 21:43:31 +0200

    Travis: Experimental! Do not use!

--- get-deps.sh
+++ get-deps.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -ex
 
-#wget http://kent.dl.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2
-#tar -xjf boost_1_56_0.tar.bz2
-#cd boost_1_56_0
-#./bootstrap.sh
-#sudo ./b2 install --with-test -d0
+wget http://kent.dl.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2
+tar -xjf boost_1_56_0.tar.bz2
+cd boost_1_56_0
+./bootstrap.sh
+sudo ./b2 install --with-test -d0
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
