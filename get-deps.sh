6b3cda837c5eeec4ea12eaa100d2895d701e69c8
Christian Neumüller <cn00@gmx.at>
2014-10-22 21:37:16 +0200

    Travis: Experimental! Do not use!

--- get-deps.sh
+++ get-deps.sh
@@ -7,6 +7,6 @@ set -ex
 #./bootstrap.sh
 #sudo ./b2 install --with-test -d0
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get install -q -y liblua5.2-dev libstdc++-4.8-dev
