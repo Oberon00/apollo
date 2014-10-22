f646f14de4a6ab8ef2347dce3aaa10e6ab7db01a
Christian Neumüller <cn00@gmx.at>
2014-10-22 20:11:07 +0200

    Travis: Fix boost installation.

--- get-deps.sh
+++ get-deps.sh
@@ -5,8 +5,6 @@ wget http://kent.dl.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.
 tar -xjf boost_1_56_0.tar.bz2
 cd boost_1_56_0
 ./bootstrap.sh
-./b2 --with-test
-BOOST_ROOT=$PWD && export BOOST_ROOT
+sudo ./b2 install --with-test
 
-sudo apt-get update
 sudo apt-get install -q -y liblua5.2-dev
