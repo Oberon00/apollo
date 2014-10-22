35bcf2ca57e92f180c3d2f7a20cbce09875f1214
Christian Neumüller <cn00@gmx.at>
2014-10-22 19:29:51 +0200

    Add experimental Travic CI support.

---
+++ get-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -ex
+
+wget http://kent.dl.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2
+tar -xjf boost_1_56_0.tar.bz2
+cd boost_1_56_0
+./bootstrap.sh
+./b2 --with-test
+BOOST_ROOT=$PWD && export BOOST_ROOT
+
+sudo apt-get update
+sudo apt-get install liblua5.2-dev
