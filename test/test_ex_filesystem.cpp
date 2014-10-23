6fe7e243bf519fbac5cf1eacf8381280b73013f4
Christian Neumüller <cn00@gmx.at>
2014-10-23 19:34:20 +0200

    Add skeleton for filesystem example.

---
+++ test/test_ex_filesystem.cpp
@@ -0,0 +1,14 @@
+#include <apollo/class_utility.hpp>
+
+#include <boost/filesystem.hpp>
+
+#include "test_prefix.hpp"
+
+
+
+BOOST_AUTO_TEST_CASE(filesytem)
+{
+    
+}
+
+#include "test_suffix.hpp"
