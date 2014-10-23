e49cdfd764a809460b8005284893b379a3ba0b11
Christian Neumüller <cn00@gmx.at>
2014-10-23 11:29:07 +0200

    create_table: Test nested cls & thistable_index.

--- test/test_class_utility.cpp
+++ test/test_class_utility.cpp
@@ -14,6 +14,15 @@ public:
     }
 };
 
+class bar_cls {
+public:
+    char const* btest()
+    {
+        ++g_n_calls;
+        return "got it";
+    }
+};
+
 } // anonymous namespace
 
 BOOST_AUTO_TEST_CASE(simple)
@@ -26,14 +35,52 @@ BOOST_AUTO_TEST_CASE(simple)
     apollo::export_classes(L, -1)
         .cls<foo_cls>("foo_cls") // Executed first
             .ctor<>()
+            .thistable_index()
             ("test", APOLLO_TO_RAW_FUNCTION(&foo_cls::test))
         .end_cls();
     lua_pop(L, 1);
 
     require_dostring(L,
         "local foo = foo_cls.new()\n"
-        "foo_cls.test(foo)");
-    BOOST_CHECK_EQUAL(g_n_calls, 1u);
+        "foo_cls.test(foo)\n"
+        "foo:test()\n"
+        "assert(foo.nonexistent == nil)\n");
+    BOOST_CHECK_EQUAL(g_n_calls, 2u);
+}
+
+BOOST_AUTO_TEST_CASE(nested)
+{
+    g_n_calls = 0;
+    luaL_requiref(L, "base", &luaopen_base, true);
+    lua_pop(L, 1);
+
+    lua_pushglobaltable(L);
+    apollo::export_classes(L, -1)
+        .cls<foo_cls>("foo_cls") // Executed first
+            .thistable_index()
+            ("test", APOLLO_TO_RAW_FUNCTION(&foo_cls::test))
+            .cls<bar_cls>("bar_cls")
+                .ctor<>()
+                .thistable_index()
+                ("btest", APOLLO_TO_RAW_FUNCTION(&bar_cls::btest))
+            .end_cls()
+            .ctor<>()
+        .end_cls();
+    lua_pop(L, 1);
+
+    require_dostring(L,
+        "local foo = foo_cls.new()\n"
+        "foo_cls.test(foo)\n"
+        "foo:test()\n"
+        "assert(foo.nonexistent == nil)\n");
+    BOOST_CHECK_EQUAL(g_n_calls, 2u);
+
+    g_n_calls = 0;
+    require_dostring(L,
+        "local bar = foo_cls.bar_cls.new()\n"
+        "assert(foo_cls.bar_cls.btest(bar) == 'got it', 'btest retval')\n"
+        "assert(bar:btest() == 'got it', 'btest retval (OO style call)')\n");
+    BOOST_CHECK_EQUAL(g_n_calls, 2u);
 }
 
 #include "test_suffix.hpp"
