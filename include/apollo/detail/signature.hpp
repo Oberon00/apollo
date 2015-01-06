43b287bab551b30926af4e0497820fc6401412c4
Christian Neumüller <cn00@gmx.at>
2015-01-06 21:11:59 +0100

    Work around MSVC's confusion w/ signature_element.

--- include/apollo/detail/signature.hpp
+++ include/apollo/detail/signature.hpp
@@ -25,6 +25,7 @@ template <class C, typename R,  typename... Args>
 std::tuple<R, C const&, Args...> signature_tuple_of_impl(
     R(C::*)(Args...) const);
 
+
 template <typename SignatureTuple>
 using arg_idxs_seq = iseq_n_t<std::tuple_size<
     typename detail::remove_qualifiers<SignatureTuple>::type>::value - 1, 1>;
@@ -33,8 +34,12 @@ template <typename F>
 using signature_tuple_of = decltype(signature_tuple_of_impl(std::declval<F>()));
 
 template <std::size_t I, typename F>
-using signature_element = typename std::tuple_element<
-    I, signature_tuple_of<F>>::type;
+struct signature_element_impl { // Woraround MSVC 12's confusion.
+    using type = typename std::tuple_element<I, signature_tuple_of<F>>::type;
+};
+
+template <std::size_t I, typename F>
+using signature_element = typename signature_element_impl<I, F>::type;
 
 template <typename F>
 using return_type_of = signature_element<0, F>;
