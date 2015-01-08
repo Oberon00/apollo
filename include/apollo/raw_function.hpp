9fcab9532b93ea3e959220a9c55ec4438dd76870
Christian Neumüller <cn00@gmx.at>
2015-01-08 16:03:54 +0100

    raw function macros: Use __VA__ARGS__ everywhere.

--- include/apollo/raw_function.hpp
+++ include/apollo/raw_function.hpp
@@ -111,15 +111,17 @@ BOOST_CONSTEXPR raw_function to_raw_function() BOOST_NOEXCEPT
     return detail::to_raw_function_impl<F, FVal>(detail::tuple_seq<cvts>());
 }
 
-#define APOLLO_TO_RAW_FUNCTION(f) apollo::to_raw_function<decltype(f), f>()
-#define APOLLO_PUSH_FUNCTION_STATIC(L, f) \
-    lua_pushcfunction(L, APOLLO_TO_RAW_FUNCTION(f))
+#define APOLLO_TO_RAW_FUNCTION(...) \
+    apollo::to_raw_function<decltype(__VA_ARGS__), __VA_ARGS__>()
+#define APOLLO_PUSH_FUNCTION_STATIC(L, ...) \
+    lua_pushcfunction(L, APOLLO_TO_RAW_FUNCTION(__VA_ARGS__))
 #define APOLLO_PUSH_FUNCTION_STATIC_WITH(L, f, rconv, ...) \
     lua_pushcfunction(L, APOLLO_MAKE_LIGHT_FUNCTION(f, rconv, __VA_ARGS__))
 
-// Because MSVC fails at deducing the type of a (variadic?) template function
-// address (i.e. decltype(&ctor_wrapper<T, Args...> fails), this function
-// is needed.
+// Because MSVC 12 (2013) fails at deducing the type of a (variadic?) template
+// function address (i.e. decltype(&ctor_wrapper<T, Args...> fails or deduces a
+// function type instead of a pointer-to-function type), this function is
+// needed.
 template <typename T, typename... Args>
 BOOST_CONSTEXPR raw_function get_raw_ctor_wrapper() BOOST_NOEXCEPT
 {
