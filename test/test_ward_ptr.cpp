0d1500fa41b922d1dede08397743b77b470cb715
Christian Neumüller <cn00@gmx.at>
2015-01-31 18:18:06 +0100

    Add tests for ward_ptr.hpp (incomplete!).

---
+++ test/test_ward_ptr.cpp
@@ -0,0 +1,27 @@
+#include <apollo/ward_ptr.hpp>
+#include <memory>
+
+#include "test_prefix.hpp"
+
+struct warded : apollo::enable_ward_ptr_from_this<warded> {};
+
+
+BOOST_AUTO_TEST_CASE(ward_ptr_basic)
+{
+    std::unique_ptr<warded> p(new warded);
+    apollo::ward_ptr<warded> wp(p.get());
+    BOOST_CHECK_EQUAL(wp.get(), p.get());
+    BOOST_CHECK_EQUAL(wp.get(), p->ref().get());
+    BOOST_CHECK(wp.valid());
+    BOOST_CHECK(!!wp);
+    p.reset();
+    BOOST_CHECK_EQUAL(wp.get(), static_cast<warded*>(nullptr));
+    BOOST_CHECK(!wp.valid());
+    BOOST_CHECK(!wp);
+
+    BOOST_CHECK_THROW(*wp, apollo::bad_ward_ptr);
+}
+
+// TODO: copy, move constructor, assignment operator; derived classes, ref()
+
+#include "test_suffix.hpp"
