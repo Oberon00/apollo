95f80590f7493807a86999c6003f929863f59b45
Christian Neumüller <cn00@gmx.at>
2015-08-22 15:19:04 +0200

    Remove Boost.Preprocessor dependency.

--- test/test_prefix.hpp
+++ test/test_prefix.hpp
@@ -9,9 +9,8 @@
 
 #include "testutil.hpp"
 
-#include <boost/preprocessor/cat.hpp>
 #include <boost/test/unit_test.hpp>
 
 BOOST_FIXTURE_TEST_SUITE(
-    BOOST_PP_CAT(BOOST_TEST_MODULE, _suite),
+    BOOST_JOIN(BOOST_TEST_MODULE, _suite),
     lstate_fixture)
